52b89d735440e3901b7c2602cab6cbc8a7a83a56
luisledzma <luisledzma@outlook.com>
2025-09-02 23:48:04 -0600

    Add Animations and shapes

---
+++ Landmarks/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Luis Antonio Ledezma Cordero on 2/9/25.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+
+       
+
+       var body: some View {
+
+           BadgeSymbol()
+               .padding(-60)
+               .rotationEffect(angle, anchor: .bottom)
+       }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
